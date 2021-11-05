2f5b3445c4ae543902797249e8f89fe03286ea09
Tony Mascate <tonymascate@hotmail.com>
2021-11-05 16:22:53 +0100

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,21 @@
+import About from './AboutPage'
+import AuthWrapper from './AuthWrapper'
+import Cart from './CartPage'
+import Home from './HomePage'
+import Error from './ErrorPage'
+import Checkout from './CheckoutPage'
+import PrivateRoute from './PrivateRoute'
+import Products from './ProductsPage'
+import SingleProduct from './SingleProductPage'
+
+export {
+    About,
+    AuthWrapper,
+    Cart,
+    Home,
+    Error,
+    Checkout,
+    PrivateRoute,
+    Products,
+    SingleProduct
+}
